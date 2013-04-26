65cfda05207d6af1f575a5c674216e54bc88e877
shanebp <shanebp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-26 11:34:38 +0000

    tag 1.1.3
    git-svn-id: https://plugins.svn.wordpress.org/bp-profile-message-ux@704139 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.1.3/loader.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: BP Profile Message UX
+Description: A BuddyPress plugin. Replaces the functionality for Public and Private buttons so that you stay on a profile page.  
+Author: shanebp
+License: GPLv2 
+Author URI: http://philogames.com/contact
+Version: 1.1.3
+*/
+
+
+if ( !defined( 'ABSPATH' ) ) exit;  
+
+define( 'BP_PROFILE_MESSAGE_UX', '1.1.3' );
+
+function bp_profile_message_ux_init() {
+    require( dirname( __FILE__ ) . '/bp-profile-message-ux.php' );
+	load_plugin_textdomain( 'bp-profile-message-ux', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );	
+}
+add_action( 'bp_init', 'bp_profile_message_ux_init' );
+
+?>
